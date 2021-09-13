3397c3f10d5f54d2cf614f6d78d697a3a2447329
gusfedec <gus.fe.c@gmail.com>
2021-09-13 16:03:37 -0300

    Splash animado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { SplashScreen } from '@capacitor/splash-screen';
 import { AnimationController } from '@ionic/angular';
+import { AnimationService } from '../shared/animation.service';
 
 @Component({
   selector: 'app-home',
@@ -9,19 +10,76 @@ import { AnimationController } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  animation: any;
+  animation1: any;
   constructor(
     private animationController: AnimationController,
-    private router: Router
+    private router: Router,
+    private animationService: AnimationService
   ) {}
 
   async ionViewDidEnter() {
     SplashScreen.hide();
+    this.animation = this.animationController
+      .create()
+      .addElement(document.querySelector('.splash'))
+      .duration(3000)
+      .iterations(Infinity)
+      .fromTo('transform', 'translateX(-350px)', 'translateX(350px)');
+    //.fromTo('opacity', '1', '0')
+
+    this.animation.play();
+
+    this.animation1 = this.animationController
+      .create()
+      .addElement(document.querySelector('.logo'))
+      .duration(1500)
+      .iterations(Infinity)
+      // .fromTo('transform', 'translateX(0px)', 'translateX(500px)')
+      // .fromTo('opacity', '1', '0.2');
+      .keyframes([
+        { offset: 0, transform: 'scale(1) rotate(0)' },
+        { offset: 0.5, transform: 'scale(1.2) rotate(360deg)' },
+        { offset: 1, transform: 'scale(1) rotate(700deg) ' },
+      ]);
+    this.animation1.play();
+
+    /* const animation2 = this.animationController
+      .create()
+      .addElement(document.querySelector('.logo'))
+      .duration(3000)
+      .iterations(Infinity)
+      .keyframes([
+        { offset: 0, background: 'red' },
+        { offset: 0.72, background: 'var(--background)' },
+        { offset: 1, background: 'green' },
+      ]);
+    await animation2.play();
+
+    const animation3 = this.animationController
+      .create()
+      .addElement(document.querySelector('.logo'))
+      .duration(3000)
+      .iterations(Infinity)
+      .keyframes([
+        { offset: 0, background: 'red' },
+        { offset: 0.72, background: 'var(--background)' },
+        { offset: 1, background: 'green' },
+      ]);
+    await animation3.play(); */
+
     const animation = this.animationController
       .create()
       .addElement(document.querySelector('#container'))
-      .duration(3000)
+      .duration(4000)
       .fromTo('opacity', '1', '0');
     await animation.play();
+
     this.router.navigateByUrl('login');
+  }
+
+  stop() {
+    //this.animationService.pararElemento();
+    this.animation.stop();
   }
 }
